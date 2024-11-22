1468b1d100ee4e3e82aeb4537a568efcf3d754c6
wangcome <58815948+wangcome@users.noreply.github.com>
2024-11-22 23:51:41 +0800

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -7,7 +7,7 @@ import { SignJWT, jwtVerify } from 'jose';
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
 let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
-let trojanPassword = `bpb-trojan`;
+let trojanPassword = `bpbb-trojan`;
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
